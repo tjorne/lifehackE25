f9455b182ce5e09f340df60f7b77ddfe09a74c3e
Jesper Tjørnelund <69388371+tjorne@users.noreply.github.com>
2025-10-07 08:57:24 +0200

    Initial commit

---
+++ src/main/java/app/controllers/TimeZonesController.java
@@ -0,0 +1,17 @@
+package app.controllers;
+
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+
+public class TimeZonesController
+{
+    public static void addRoutes(Javalin app)
+    {
+        app.get("/timezones", ctx -> index(ctx));
+    }
+
+    private static void index(Context ctx)
+    {
+        ctx.render("/timezones/index.html");
+    }
+}
