8dd1b035b5e44767b822f3cbf3faca51b996068a
olivermjmj <olivermjmj@outlook.dk>
2025-10-08 15:13:42 +0200

    update of user.

--- src/main/java/app/entities/User.java
+++ src/main/java/app/entities/User.java
@@ -5,7 +5,6 @@ public class User
     private int userId;
     private String userName;
     private String password;
-    private String role;
 
     public User(int userId, String userName, String password)
     {
@@ -36,7 +35,6 @@ public class User
                 "userId=" + userId +
                 ", userName='" + userName + '\'' +
                 ", password='" + password + '\'' +
-                ", role='" + role + '\'' +
                 '}';
     }
 }
