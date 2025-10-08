61116a0c70bc3d700470ead76418bd77479a498b
luke-dkk <luke_persson@yahoo.dk>
2025-10-08 15:44:09 +0200

    This is for testing purposes, disregard

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -11,7 +11,7 @@ public class Main
     private static final String USER = "postgres";
     private static final String PASSWORD = "postgres";
     private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
-    private static final String DB = "lifehack";
+    private static final String DB = "references";
 
     private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
 
@@ -25,9 +25,9 @@ public class Main
         }).start(7070);
 
         // Routing
-        app.get("/", ctx -> ctx.render("index.html"));
+        app.get("/", ctx -> ctx.render("references/index.html"));
         UserController.addRoutes(app);
         TimeZonesController.addRoutes(app);
-
+        ReferencesController.addRoutes(app, connectionPool);
     }
 }
---
+++ src/main/java/app/entities/references/Books.java
@@ -0,0 +1,65 @@
+package app.entities.references;
+
+public class Books {
+    private int id;
+    private String title;
+    private int year;
+    private int field_id;
+    private String description;
+
+
+    public Books(int id, String title, int year, int field_id, String description) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.field_id = field_id;
+        this.description = description;
+    }
+
+    public Books( int id, String title, String description){
+        this.id = id;
+        this.title = title;
+        this.description = description;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getField_id() {
+        return field_id;
+    }
+
+    public void setField_id(int field_id) {
+        this.field_id = field_id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
