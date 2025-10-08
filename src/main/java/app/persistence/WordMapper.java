227b53d19612f98d95f3fbd7c91e4d5abdfe6876
EmilXavierVT <emilxavierthorsen@gmail.com>
2025-10-08 10:48:56 +0200

    word mapper has been initialized

--- src/main/java/app/persistence/WordMapper.java
+++ src/main/java/app/persistence/WordMapper.java
@@ -1,7 +1,47 @@
 package app.persistence;
 
+import app.entities.Word;
+import app.exceptions.DatabaseException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Random;
+
 public class WordMapper {
 
-//     lytter til den første character der bliver trykket på
+    //     lytter til den første character der bliver trykket på
+    public Word getWord() throws DatabaseException {
 
+        Random rdm = new Random(getLengthOfList());
+        String sql = "select word from word where id = ?";
+        try (
+                Connection connection = ConnectionPool.getInstance().getConnection();
+                PreparedStatement ps = connection.prepareStatement(sql)
+        ) {
+            ps.setInt(1, rdm.nextInt(getLengthOfList()));
+            ResultSet rs = ps.executeQuery();
+            rs.next();
+            return new Word(rs.getString(1));
+        } catch (SQLException e) {
+            throw new DatabaseException("DB fejl", e.getMessage());
+        }
+    }
+
+
+    private int getLengthOfList() throws DatabaseException {
+        ConnectionPool connectionPool = ConnectionPool.getInstance();
+        String sql = "select count(*) from words";
+        try (
+                Connection connection = connectionPool.getConnection();
+                PreparedStatement ps = connection.prepareStatement(sql)
+        ) {
+            ResultSet rs = ps.executeQuery();
+            rs.next();
+            return rs.getInt(1);
+        } catch (SQLException e) {
+            throw new DatabaseException("DB fejl", e.getMessage());
+        }
+    }
 }
