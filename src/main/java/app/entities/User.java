c314ee2cf3aead8c4e9ab3b01817c6e176e9c121
cph-ds303 <cph-ds303@stud.ek.dk>
2025-10-10 11:54:49 +0200

    Update User.java
    Reverted back the changes to the original User class. However I did leave  the " public User(int userId, String userName, String password)" constructer in place, as we're using it in our part of the code, And I dont think it will have an effect on other groups.

--- src/main/java/app/entities/User.java
+++ src/main/java/app/entities/User.java
@@ -1,31 +1,25 @@
-package app.entities;
-
-public class User
-{
     private int userId;
     private String userName;
     private String password;
+    private String role;
 
+    public User(int userId, String userName, String password, String role)
     public User(int userId, String userName, String password)
     {
         this.userId = userId;
         this.userName = userName;
         this.password = password;
+        this.role = role;
     }
 
     public int getUserId()
-    {
-        return userId;
-    }
-
-    public String getUserName()
-    {
-        return userName;
-    }
-
-    public String getPassword()
-    {
+@@ -30,19 +28,13 @@ public String getPassword()
         return password;
+    }
+
+    public String getRole()
+    {
+        return role;
     }
 
     @Override
@@ -35,6 +29,7 @@ public class User
                 "userId=" + userId +
                 ", userName='" + userName + '\'' +
                 ", password='" + password + '\'' +
+                ", role='" + role + '\'' +
                 '}';
     }
 }
