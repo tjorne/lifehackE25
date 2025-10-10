2ed7b9e390c92d529be4876e651fc349d7165c54
cph-ds303 <cph-ds303@cphbusiness.dk>
2025-10-10 14:05:39 +0200

    Fixed User to it's original state, removed random non eligible code blocks.

--- src/main/java/app/entities/User.java
+++ src/main/java/app/entities/User.java
@@ -1,10 +1,13 @@
+package app.entities;
+
+public class User
+{
     private int userId;
     private String userName;
     private String password;
     private String role;
 
     public User(int userId, String userName, String password, String role)
-    public User(int userId, String userName, String password)
     {
         this.userId = userId;
         this.userName = userName;
@@ -13,7 +16,17 @@
     }
 
     public int getUserId()
-@@ -30,19 +28,13 @@ public String getPassword()
+    {
+        return userId;
+    }
+
+    public String getUserName()
+    {
+        return userName;
+    }
+
+    public String getPassword()
+    {
         return password;
     }
 
