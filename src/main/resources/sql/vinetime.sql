6a97bd569b7232f27de842d4548296cd8576aeae
Tesselation93 <tessvanslyckspam@gmail.com>
2025-10-15 12:52:59 +0200

    Vinetime - added sql file

--- src/main/resources/sql/vinetime.sql
+++ src/main/resources/sql/vinetime.sql
@@ -1,7 +1,15 @@
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(50) DEFAULT 'USER' NOT NULL
+);
+
 CREATE TABLE vt_session (
     session_id SERIAL PRIMARY KEY,
     user_id INTEGER NOT NULL,
-    session_type VARCHAR (50) NOT NULL,
-    created_at TIMESTAMP NOT NULL CURRENT_TIMESTAMP,
-    ended_at TIMESTAMP
+    session_type VARCHAR(50) NOT NULL,
+    duration_seconds INTEGER NOT NULL,
+    completed_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
